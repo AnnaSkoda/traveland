1df37b499695f2754d5953403044d5f698536013
AnnaSkoda <zbaraz77@gmail.com>
2024-03-01 22:20:11 +0200

    add adaptive sections news&footer

--- js/main.js
+++ js/main.js
@@ -5,7 +5,51 @@ $(document).ready(function () {
     slidesToScroll: 1,
     dots:true,
     prevArrow:"<button type='button' class='btn-slick-prev btn btn-slider'><img src='img/Special_Offers/arrows-left.svg' alt='prev'></button>",
-    nextArrow:"<button type='button' class='btn-slick-next btn btn-slider'><img src='img/Special_Offers/arrows-right.svg' alt='next'></button>"
+    nextArrow:"<button type='button' class='btn-slick-next btn btn-slider'><img src='img/Special_Offers/arrows-right.svg' alt='next'></button>",
+    responsive: [
+        {
+            breakpoint: 1401,
+            settings: {
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                dots:true,
+            }
+        },
+        {
+            breakpoint: 1101,
+            settings: {
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                dots:true,
+            }
+        },
+        {
+            breakpoint: 851,
+            settings: {
+                infinite: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 2000,
+            }
+        },
+        {
+            breakpoint: 551,
+            settings: {
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 2000,
+            }
+        },
+    ]
     });
 
     $(".mobile-menu").click(function () {
